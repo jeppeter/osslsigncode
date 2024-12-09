79288202cb130dec15203b58b1f6c5c385954eb1
jeppeter <jeppeter@gmail.com>
2024-12-09 17:16:33 +0800

    to make openssl path export

--- runossl.sh
+++ runossl.sh
@@ -2,5 +2,6 @@
 
 scriptfile=`readlink -f $0`
 scriptdir=`dirname $scriptfile`
-export LD_LIBRARY_PATH=/home/bt/sources/openssl/openssl-3.0.2
+#export LD_LIBRARY_PATH=/home/bt/sources/openssl/openssl-3.0.2
+export LD_LIBRARY_PATH=/home/bt/sources/openssl/
 $scriptdir/osslsigncode $@
